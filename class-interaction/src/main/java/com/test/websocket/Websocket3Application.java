81eb1ed96af2e652ffc13d8fb88e8153b988a27d
jichuansi <6834403@qq.com>
2018-09-21 11:05:52 +0800

    课堂互动

---
+++ class-interaction/src/main/java/com/test/websocket/Websocket3Application.java
@@ -0,0 +1,12 @@
+package com.test.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websocket3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Websocket3Application.class, args);
+	}
+}
