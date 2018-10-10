1f89fdaae1e8a650acfacbc8cb135507cc212041
jichuansi <6834403@qq.com>
2018-10-10 16:56:55 +0800

    课堂统计服务

---
+++ course-statisticsservice/src/main/java/com/jichuangsi/school/SchoolCourseStatisticsserviceApplication.java
@@ -0,0 +1,12 @@
+package com.jichuangsi.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolCourseStatisticsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolCourseStatisticsserviceApplication.class, args);
+	}
+}
