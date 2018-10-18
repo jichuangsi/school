0f5669327621dca20ef0e9a46061c42d85f9d0b0
jichuansi <6834403@qq.com>
2018-10-18 11:39:00 +0800

    增加swaggerUI

--- course-statisticsservice/src/main/java/com/jichuangsi/school/statistics/controller/StatisticsInfoQueryController.java
+++ course-statisticsservice/src/main/java/com/jichuangsi/school/statistics/controller/StatisticsInfoQueryController.java
@@ -14,22 +14,34 @@ import com.jichuangsi.school.statistics.model.CourseStatisticsModel;
 import com.jichuangsi.school.statistics.model.QuestionStatisticsListModel;
 import com.jichuangsi.school.statistics.service.ICourseStatisticsService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 /**
  * @author huangjiajun
  *
  */
 @RestController
+@Api("StatisticsInfoQueryController相关的api")
 public class StatisticsInfoQueryController {
 
 	@Resource
 	private ICourseStatisticsService courseStatisticsService;
 
+	@ApiOperation(value = "根据课堂id查询课堂统计信息", notes = "")
+	@ApiImplicitParams({
+			@ApiImplicitParam(paramType = "path", name = "courseId", value = "课堂ID", required = true, dataType = "String") })
 	@GetMapping("/getCourseStatistics/{courseId}")
 	public ResponseModel<CourseStatisticsModel> getCourseStatistics(@PathVariable String courseId) {
 		CourseStatisticsModel model = courseStatisticsService.getCourseStatistics(courseId);
 		return ResponseModel.sucess("", model);
 	}
 
+	@ApiOperation(value = "根据课堂id查询课堂中题目的统计信息", notes = "")
+	@ApiImplicitParams({
+			@ApiImplicitParam(paramType = "path", name = "courseId", value = "课堂ID", required = true, dataType = "String") })
 	@GetMapping("/getQuestionStatisticsList/{courseId}")
 	public ResponseModel<QuestionStatisticsListModel> getQuestionStatisticsList(@PathVariable String courseId) {
 		QuestionStatisticsListModel model = courseStatisticsService.getQuestionStatisticsList(courseId);
