a00bedc9809680a368b0a7549e1819766957a464
jichuansi <6834403@qq.com>
2018-10-30 15:47:29 +0800

    Swagger2更新

--- course-statisticsservice/src/main/java/com/jichuangsi/school/statistics/config/Swagger2.java
+++ course-statisticsservice/src/main/java/com/jichuangsi/school/statistics/config/Swagger2.java
@@ -3,6 +3,7 @@
  */
 package com.jichuangsi.school.statistics.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,10 +22,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2 {
+
+	@Value("${custom.swagger.enable}")
+	private boolean enableSwagger;
+
 	// swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
 	@Bean
 	public Docket createRestApi() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+		return new Docket(DocumentationType.SWAGGER_2).enable(enableSwagger).apiInfo(apiInfo()).select()
 				// 为当前包路径
 				.apis(RequestHandlerSelectors.basePackage("com.jichuangsi.school.statistics.controller"))
 				.paths(PathSelectors.any()).build();
