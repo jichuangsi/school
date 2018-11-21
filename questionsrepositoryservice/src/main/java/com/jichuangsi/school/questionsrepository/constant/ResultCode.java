a98fe7ece2a2b8d7ba3db57295542e2f2be4412c
jichuangsi <school@jichuangsi>
2018-11-21 13:29:33 +0800

    [jiewen.wu]新增常量

---
+++ questionsrepositoryservice/src/main/java/com/jichuangsi/school/questionsrepository/constant/ResultCode.java
@@ -0,0 +1,5 @@
+package com.jichuangsi.school.questionsrepository.constant;
+
+public class ResultCode extends com.jichuangsi.microservice.common.constant.ResultCode{
+	public static final String PHP_CORRECT_CODE = "0";
+}
