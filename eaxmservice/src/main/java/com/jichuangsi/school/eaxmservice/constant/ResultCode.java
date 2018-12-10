dd0d03a08c0158654a454b95e8f8a0e8e9ba9b2f
jichuangsi <school@jichuangsi>
2018-12-10 15:36:12 +0800

    [jiewen.wu]删除eaxmservice

--- eaxmservice/src/main/java/com/jichuangsi/school/eaxmservice/constant/ResultCode.java
+++
@@ -1,6 +0,0 @@
-package com.jichuangsi.school.eaxmservice.constant;
-
-public final class ResultCode extends com.jichuangsi.microservice.common.constant.ResultCode{
-    public final static String PARAM_PATTERN_ERR = "参数格式错误";
-    public final static String EAXM_EXIST_ERR = "试题不存在";
-}
--- eaxmservice/src/main/java/com/jichuangsi/school/eaxmservice/exception/EaxmException.java
+++
@@ -1,8 +0,0 @@
-package com.jichuangsi.school.eaxmservice.exception;
-
-public class EaxmException extends Exception {
-    private String msg;
-    public EaxmException(String msg){
-        this.msg = msg;
-    }
-}
