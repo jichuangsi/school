e73ca359945d07be6086a1f88fe9fcfd19be208a
jichuangsi <school@jichuangsi>
2018-12-11 10:20:03 +0800

    [jiewen.wu]增加传输新对象

---
+++ userservice/src/main/java/com/jichuangsi/school/user/model/transfer/TransferStudent.java
@@ -0,0 +1,26 @@
+package com.jichuangsi.school.user.model.transfer;
+
+public class TransferStudent {
+
+    private String studentId;
+    private String studentName;
+
+    public TransferStudent(){}
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+}
+
