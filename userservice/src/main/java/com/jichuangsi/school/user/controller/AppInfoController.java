c0d1e0f0f774e8ff0a9a2452dadb872ed2350ded
jichuangsi <school@jichuangsi>
2019-02-19 15:28:22 +0800

    [jiewen.wu]app更新接口增加token

--- userservice/src/main/java/com/jichuangsi/school/user/controller/AppInfoController.java
+++ userservice/src/main/java/com/jichuangsi/school/user/controller/AppInfoController.java
@@ -1,12 +1,14 @@
 package com.jichuangsi.school.user.controller;
 
 import com.jichuangsi.microservice.common.model.ResponseModel;
+import com.jichuangsi.microservice.common.model.UserInfoForToken;
 import com.jichuangsi.school.user.exception.UserServiceException;
 import com.jichuangsi.school.user.model.app.AppInfoModule;
 import com.jichuangsi.school.user.model.app.AppInfoQueryModule;
 import com.jichuangsi.school.user.service.IAppInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +24,7 @@ public class AppInfoController {
     //根据账号密码登陆
     @ApiOperation(value = "获取最新app信息", notes = "")
     @PostMapping("/getAppInfo")
-    public ResponseModel<AppInfoModule> login(@RequestBody AppInfoQueryModule appInfoQueryModule) throws UserServiceException{
+    public ResponseModel<AppInfoModule> login(@ModelAttribute UserInfoForToken userInfo, @RequestBody AppInfoQueryModule appInfoQueryModule) throws UserServiceException{
         return ResponseModel.sucess("", IAppInfoService.fetchLastestAppInfo(appInfoQueryModule));
     }
 }
