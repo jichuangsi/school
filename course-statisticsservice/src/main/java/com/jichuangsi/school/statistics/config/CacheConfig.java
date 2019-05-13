76445105f1fa99a9797b78ef092c8c6985c80c9a
jichuangsi <school@jichuangsi>
2019-05-13 10:14:14 +0800

    [jiewen.wu]增加定时刷新统计缓存

--- course-statisticsservice/src/main/java/com/jichuangsi/school/statistics/config/CacheConfig.java
+++ course-statisticsservice/src/main/java/com/jichuangsi/school/statistics/config/CacheConfig.java
@@ -1,15 +1,66 @@
 package com.jichuangsi.school.statistics.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.jichuangsi.microservice.common.model.UserInfoForToken;
 import com.jichuangsi.school.statistics.model.classType.SearchStudentKnowledgeModel;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 
 @Configuration
 public class CacheConfig {
+
+    private static final int DEFAULT_MAXSIZE = 10000;
+    private static final int DEFAULT_TTL = 600;
+
+    public enum Caches{
+        hotpointCache(604800,1000),          //有效期一周 , 最大容量1000
+        statisticsCache(43200,1000);        //有效期半天 , 最大容量1000
+
+        Caches() {
+        }
+        Caches(int ttl) {
+            this.ttl = ttl;
+        }
+        Caches(int ttl, int maxSize) {
+            this.ttl = ttl;
+            this.maxSize = maxSize;
+        }
+        private int maxSize=DEFAULT_MAXSIZE;    //最大數量
+        private int ttl=DEFAULT_TTL;        //过期时间（秒）
+        public int getMaxSize() {
+            return maxSize;
+        }
+        public int getTtl() {
+            return ttl;
+        }
+    }
+
+    @Bean
+    @Primary
+    public CacheManager caffeineCacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        ArrayList<CaffeineCache> caches = new ArrayList<CaffeineCache>();
+        for(Caches c : Caches.values()){
+            caches.add(new CaffeineCache(c.name(),
+                    Caffeine.newBuilder().recordStats()
+                            .expireAfterWrite(c.getTtl(), TimeUnit.SECONDS)
+                            .maximumSize(c.getMaxSize())
+                            .build())
+            );
+        }
+        cacheManager.setCaches(caches);
+        return cacheManager;
+    }
 
     @Bean(value = "classStatisticsKeyGenerator")
     public KeyGenerator classStatisticsKeyGenerator() {
